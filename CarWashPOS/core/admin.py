25e6a44af33c7ecf536c6070d5732997d0a09f7d
Georgi Vladimirov <v.v.georgi@gmail.com>
2026-03-16 23:12:10 +0200

    core models

--- CarWashPOS/core/admin.py
+++ CarWashPOS/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Location,
+    VehicleType,
+    ServicePrice,
+    ServiceType,
+    Service,
+    VehicleBrand,
+    EmployeePosition,
+    Employee,
+    Subscriber,
+    CalendarEvent,
+)
+
+admin.site.register(Location)
+admin.site.register(VehicleType)
+admin.site.register(ServicePrice)
+admin.site.register(ServiceType)
+admin.site.register(Service)
+admin.site.register(VehicleBrand)
+admin.site.register(EmployeePosition)
+admin.site.register(Employee)
+admin.site.register(Subscriber)
+admin.site.register(CalendarEvent)
--- CarWashPOS/core/models.py
+++ CarWashPOS/core/models.py
@@ -1,3 +1,103 @@
 from django.db import models
+from decimal import Decimal
+from common.models import BaseModel
 
-# Create your models here.
+
+class Location(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    short_name = models.CharField(max_length=4, unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.short_name}"
+
+
+class VehicleType(BaseModel):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ServiceType(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+    name_BG = models.CharField(max_length=100, unique=True, default="")
+    selectivity = models.IntegerField(default=1)
+    order = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return f"{self.name} | Active: {self.is_active}"  # This will display the service type in the admin
+
+
+class Service(BaseModel):
+    name = models.CharField(max_length=200, unique=True)
+    service_type = models.ForeignKey(
+        ServiceType,
+        on_delete=models.CASCADE,
+        related_name="services",
+        blank=True,
+        null=True,
+    )
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} | Active: {self.is_active}"  # This will display the service name in the admin
+
+
+class ServicePrice(BaseModel):
+    location = models.ManyToManyField(Location, related_name="service_prices")
+    vehicle_type = models.ForeignKey(VehicleType, on_delete=models.CASCADE)
+    service = models.ForeignKey("Service", on_delete=models.CASCADE, related_name="service_prices")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        locations = ", ".join([loc.name for loc in self.location.all()])
+        return f"{self.service.name} - {self.vehicle_type.name} @ {locations}: {self.amount} | Active: {self.is_active}"
+
+
+class VehicleBrand(BaseModel):
+    brand = models.CharField(max_length=50, unique=True)
+    number_sort = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.number_sort}. {self.brand}"
+
+
+class EmployeePosition(BaseModel):
+    position = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.position} | Active: {self.is_active}"
+
+
+class Employee(BaseModel):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_id = models.CharField(max_length=15, unique=True)
+    position = models.ForeignKey(EmployeePosition, on_delete=models.CASCADE, related_name="employees")
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="employees")
+    salary_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=Decimal(0))
+    bonus_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=Decimal(0))
+
+    def __str__(self) -> str:
+        return f"{self.employee_id} ({self.first_name} {self.last_name}) | {self.position.position} | {self.location.short_name} | Active: {self.is_active}"
+
+
+class Subscriber(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=Decimal(0))
+    location = models.ManyToManyField(Location, related_name="subscribers")
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.discount_percentage}% - Active: {self.is_active}"
+
+
+class CalendarEvent(BaseModel):
+    date = models.DateField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, unique_for_date="date")
+
+    class Meta:
+        unique_together = ("date", "location")
+
+    def __str__(self) -> str:
+        return f"{self.date} - {self.location.name} - Active: {self.is_active}"
