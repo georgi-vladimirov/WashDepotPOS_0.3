6352818b300700343c66020c3fe10d7c9f32ab7b
Georgi Vladimirov <v.v.georgi@gmail.com>
2026-03-30 20:45:14 +0300

    Add POST handling to AddExpence view
    Import save_expence and, on POST, fetch cal_event_id from the session
    and return 400 if missing. Bind AddExpenceForm(date=cal_event),
    call form.save(commit=False) and pass the instance to save_expence,
    return 200 on success or re-render the form on validation errors

--- CarWashPOS/expences/views.py
+++ CarWashPOS/expences/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .selectors import get_expences_by_cal_event
 from core.selectors import get_cal_event_by_id
 from .forms import AddExpenceForm
+from .services import save_expence
 
 
 class ExpencesOverview(LoginRequiredMixin, View):
@@ -27,4 +28,16 @@ class AddExpence(LoginRequiredMixin, View):
             return HttpResponse("No calendar event selected", status=http.HTTPStatus.BAD_REQUEST)
         form = AddExpenceForm(date=cal_event)
 
+        return render(request, 'expences/costs_entry.html', {'form': form})
+
+    def post(self, request):
+        cal_event_id = request.session.get("cal_event_id")
+        cal_event = get_cal_event_by_id(cal_event_id=cal_event_id)
+        if not cal_event:
+            return HttpResponse("No calendar event selected", status=http.HTTPStatus.BAD_REQUEST)
+        form = AddExpenceForm(request.POST, date=cal_event)
+        if form.is_valid():
+            expence = form.save(commit=False)
+            save_expence(expence=expence)
+            return HttpResponse("Expence added successfully", status=http.HTTPStatus.OK)
         return render(request, 'expences/costs_entry.html', {'form': form})
