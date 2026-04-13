d3329622fe04417a653afd88b306998ab0db0b6e
Georgi Vladimirov <v.v.georgi@gmail.com>
2026-04-13 17:32:40 +0300

    Add ensure_superuser command and run in build

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 cd CarWashPOS
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput
+python manage.py ensure_superuser
